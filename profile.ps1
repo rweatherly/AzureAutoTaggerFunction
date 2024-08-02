0f52fdbfb7594c62570e6e33937ccb10e6333ac3
RyanMenVIP <93164311+RyanMenVIP@users.noreply.github.com>
2024-08-02 13:30:18 -0700

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -11,12 +11,14 @@
 
 # Authenticate with Azure PowerShell using MSI.
 # Remove this if you are not planning on using MSI or Azure PowerShell.
+Import-Module Az.Accounts
+Import-Module Az.Resources
+
 if ($env:MSI_SECRET) {
     Disable-AzContextAutosave -Scope Process | Out-Null
     Connect-AzAccount -Identity -AccountId $env:AZURE_CLIENT_ID
 }
-Import-Module Az.Accounts
-Import-Module Az.Resources
+
 # Uncomment the next line to enable legacy AzureRm alias in Azure PowerShell.
 # Enable-AzureRmAlias
 
